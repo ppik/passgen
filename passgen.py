23245f48f071bce96aaaf0a337d452aeb1c77e85
ppik <piksarv@gmail.com>
2017-04-06 22:38:11 +0300

    Add command line argument parser

--- passgen.py
+++ passgen.py
@@ -1,11 +1,22 @@
+import argparse
 import os
 import random
 
 import chardet
 
-table = {ord(x): '' for x in ('\n', '|')}
+table = {ord(x): '' for x in ('|')}
 
-def main(n=5):
+def parse_args():
+    """Create argument parser and parse command line arguments."""
+    parser = argparse.ArgumentParser(description='A simple random word picker')
+    parser.add_argument('-n', type=int, default=4,
+                        help='number of random words to pick')
+    parser.add_argument('-m', '--max', type=int,
+                        help='maximum length of a word')
+
+    return vars(parser.parse_args())
+
+def main(n=None, max=None):
     data = []
     for fname in os.listdir('./data'):
         if fname.startswith('_'):
@@ -18,12 +29,24 @@ def main(n=5):
 
         with open(f'./data/{fname}', encoding=encoding) as f:
             for line in f:
-                w = line.translate(table)
+                # Strip whitespace
+                w = line.strip()
+
+                # Remove extra characters
+                w = w.translate(table)
+
+                # Skip long words if max length given
+                if max is not None and len(w) > max:
+                    continue
+
                 data.append(w)
 
-    sel = (random.choice(data) for x in range(n))
+    sel = random.choices(data, k=n)
 
     print(' '.join(sel))
 
 if __name__ == '__main__':
-    main()
+    # Parse arguments
+    args = parse_args()
+
+    main(**args)
